1df9969ea57e00c388f55a62abf0788ad3e7816b
Dominique Albright <58437528+Dominique321@users.noreply.github.com>
2020-01-16 11:08:33 -0500

    Update teamWZ.py

--- teamWZ.py
+++ teamWZ.py
@@ -19,7 +19,7 @@ def move(my_history, their_history, my_score, their_score):
     # Decide whether to return 'c' or 'b'.
     
     return 'c'
-
+help me
     
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
